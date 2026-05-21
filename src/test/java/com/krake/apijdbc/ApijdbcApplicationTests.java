71c80e9a82106a4bc30a8e7ea6fd2d7169214a4d
BrayanNarvaez-25 <brayann251107g@gmail.com>
2026-05-20 19:46:55 -0500

    BN: Primer Commit

---
+++ src/test/java/com/krake/apijdbc/ApijdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.krake.apijdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApijdbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
